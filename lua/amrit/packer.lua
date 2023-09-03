e72519a7115682c38ff8b3a5a33b12ae75d11ec1
Amritanshu Pandey <email@amritanshu.in>
2023-09-03 19:20:05 +1000

    Add vim fugitive

--- lua/amrit/packer.lua
+++ lua/amrit/packer.lua
@@ -17,4 +17,5 @@ return require('packer').startup(function(use)
   use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('theprimeagen/harpoon')
   use 'mbbill/undotree'
+  use 'tpope/vim-fugitive'
 end)
